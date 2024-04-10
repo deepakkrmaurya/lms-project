d180ed4ff1c531c08f7f0934a30048a612a458d4
deepakkrmaurya <deepakmaurya2211@gmail.com>
2024-04-10 21:54:01 +0530

    first commit

---
+++ src/components/WabyBackground.jsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+import { WavyBackground } from "@/components/ui/wavy-background";
+
+const WabyBackground = () => {
+    return (
+        <div className=" relative h-[30rem] overflow-hidden flex items-center justify-center">
+            <WavyBackground className="w-full h-[30rem] max-w-7xl mx-auto flex flex-col items-center justify-center">
+                <p
+                    className=' mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white  mb-8'
+                >Meer Our Instructors</p>
+                <p className=" text-base md:text-lg text-white text-center mb-4">Discover the talented professions who will guid your musicals jorney</p>
+                <div className=" flex flex-row items-center justify-center w-full">
+                      fff
+                </div>
+            </WavyBackground>
+        </div>
+    )
+}
+
+export default WabyBackground
